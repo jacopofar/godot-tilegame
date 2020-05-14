95c7994e00f2f1b172d5d77f5f130ca0e8646c76
Jacopo Farina <jacopo.farina@flixbus.com>
2020-05-14 19:10:41 +0200

    First draft of integration with GodotTIE

--- HUD/ComicBubble.gd
+++ HUD/ComicBubble.gd
@@ -6,7 +6,11 @@ func say(target: Node2D, text: String):
 	comic.offset = target.position
 	target.add_child(comic)
 	comic.get_node("AnimationPlayer").play("fadeout")
-
+	var tie = get_node("/root/Main/TextConvCanvas/ColorRect/TextInterfaceEngine")
+	tie.set_color(Color(0,0,0))
+	tie.buff_text("\n")
+	tie.buff_text(text, 0.01)
+	tie.set_state(tie.STATE_OUTPUT)
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
